cc2a0b9a4f5b1a8d0944a93f0df5b5952d85a9f7
Maarten Baertsoen <maarten.baertsoen@telenet.be>
2014-12-29 11:52:50 +0100

    First draft

---
+++ AmpersandApproach/build.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: Run Cleanup  
+call:cleanup
+
+:: Run pdflatex -&gt; bibtex -&gt; pdflatex -&gt; pdflatex  
+pdflatex Parsing
+pdflatex Parsing
+:: bibtex  Parsing
+makeglossaries Parsing
+pdflatex Parsing
+pdflatex Parsing
+@cls && (cat Parsing.log | grep -i Warn)
+pause
+
+:: Run Cleanup
+call:cleanup
+
+:: Cleanup Function
+:cleanup  
+:: del /q *.dvi
+:: del /q *.out
+:: del /q *.log
+:: del /q *.aux
+:: del /q *.bbl
+:: del /q *.blg
+:: del /q *.brf
+del /q *.toc
+
+goto:eof
