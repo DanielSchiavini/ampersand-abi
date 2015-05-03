d6c8e5bb05ee319f0382b508a6bfa608500376fa
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-05-03 17:58:34 +0200

    Initial version of the document for phase 3b (research context). This version includes only an initial introduction.

---
+++ ResearchContext/build.bat
@@ -0,0 +1,33 @@
+@echo off
+
+:: Run Cleanup  
+call:cleanup
+
+:: Run pdflatex -&gt; bibtex -&gt; pdflatex -&gt; pdflatex  
+pdflatex ResearchContext>nul
+bibtex  ac
+bibtex  nac
+pdflatex ResearchContext>nul
+pdflatex ResearchContext>nul
+makeglossaries ResearchContext
+pdflatex ResearchContext>nul
+find "Warning" ResearchContext.log
+where bibtex>nul         || echo Error: Could not find bibtex.exe in the PATH
+where makeglossaries>nul || echo Error: Could not find makeglossaries.exe in the PATH
+pause
+
+:: Run Cleanup
+call:cleanup
+
+:: Cleanup Function
+:cleanup  
+:: del /q *.dvi
+:: del /q *.out
+:: del /q *.log
+:: del /q *.aux
+:: del /q *.bbl
+:: del /q *.blg
+:: del /q *.brf
+del /q *.toc
+
+goto:eof
