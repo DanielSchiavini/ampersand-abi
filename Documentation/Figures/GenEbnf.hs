8082bfcf0cec8be68ebb32ed77c6589d9b743aec
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-06-03 15:15:21 +0200

    Writing design and improving EBNF

--- Documentation/Figures/GenEbnf.hs
+++ Documentation/Figures/GenEbnf.hs
@@ -5,11 +5,17 @@ import System.IO
 import Data.Text(pack, unpack, splitOn)
 
 cleanup :: String -> String
-cleanup ('_':rest) = "\\_" ++ cleanup rest
-cleanup ('#':rest) = "\\#" ++ cleanup rest
-cleanup ('\\':rest) = "\\textbackslash{}" ++ cleanup rest
-cleanup (x:rest) = (x : cleanup rest)
-cleanup [] = []
+cleanup txt = clean txt False
+  -- adds a second parameter to check if the quotes are start or end quotes
+  where clean ('_':rest)  iq    = "\\_" ++ clean rest iq
+        clean ('#':rest)  iq    = "\\#" ++ clean rest iq
+        clean ('{':rest)  iq    = "\\{" ++ clean rest iq
+        clean ('}':rest)  iq    = "\\}" ++ clean rest iq
+        clean ('\\':rest) iq    = "\\textbackslash{}" ++ clean rest iq
+        clean ('\'':rest) False = ('`' : clean rest True)
+        clean ('\'':rest) True  = ('\'' : clean rest False)
+        clean (x:rest)    iq    = (x : clean rest iq)
+        clean []          _     = []
 
 printEbnfRule :: String -> String
 printEbnfRule x =
@@ -18,8 +24,7 @@ printEbnfRule x =
         prod = cleanup (unpack pr)
     in unlines [ " \\begin{figure}[H]"
                , "  \\centering"
-               , "  \\includegraphics[resolution=120,max size={\\textwidth}{\\textheight}]"
-               , "  {Figures/Ebnf/" ++ name ++ "}"
+               , "  \\includegraphics[resolution=120,max size={\\textwidth}{\\textheight}]{Figures/Ebnf/" ++ name ++ "}"
                , "  \\caption*{\\texttt{" ++ name ++ " \\small::= " ++ prod ++ "}}"
                , "  \\label{fig:ebnf-" ++ name ++ "}"
                , " \\end{figure}"
