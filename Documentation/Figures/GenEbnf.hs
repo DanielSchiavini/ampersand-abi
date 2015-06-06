08216f64e2bb660c804b6279e7dcb1551bfcfd6f
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-06-06 13:42:39 +0200

    Extracting EBNF in Haskell

--- Documentation/Figures/GenEbnf.hs
+++ Documentation/Figures/GenEbnf.hs
@@ -1,8 +1,11 @@
 module GenEbnf where
 
-import Debug.Trace
+import Control.Monad
+import Data.List
+import Data.String.Utils
+import Data.Text (pack, unpack, Text, splitOn)
+import System.Directory
 import System.IO
-import Data.Text(pack, unpack, splitOn)
 
 cleanup :: String -> String
 cleanup txt = clean txt False
@@ -17,11 +20,15 @@ cleanup txt = clean txt False
         clean (x:rest)    iq    = (x : clean rest iq)
         clean []          _     = []
 
+splitRule :: String -> (String, String)
+splitRule x = f $ splitOn (pack " ::=") (pack x)
+    where f :: [Text] -> (String, String)
+          f (nm:pr:[]) = (unpack nm, cleanup (unpack pr))
+          f _          = ("Invalid Rule", x)
+
 printEbnfRule :: String -> String
 printEbnfRule x =
-    let (nm:pr:[]) = splitOn (pack " ::=") (pack x)
-        name = unpack nm
-        prod = cleanup (unpack pr)
+    let (name,prod) = splitRule x
     in unlines [ " \\begin{figure}[H]"
                , "  \\centering"
                , "  \\includegraphics[resolution=120,max size={\\textwidth}{\\textheight}]{Figures/Ebnf/" ++ name ++ "}"
@@ -30,8 +37,32 @@ printEbnfRule x =
                , " \\end{figure}"
                ]
 
+getFiles :: FilePath -> String -> IO [FilePath]
+getFiles dir ext =
+    do contents <- getDirectoryContents dir
+       let valid = filter (endswith ext) contents
+       let paths = map (dir ++) valid
+       files <- filterM doesFileExist paths
+       return $ sort files
+
+extractRules :: String -> [String]
+extractRules file =
+    let trim  = map strip $ lines file
+        filt = filter (startswith "--- ") trim
+    in  map (drop 4) filt
+
+getFileRules :: FilePath -> IO [String]
+getFileRules fn = do file  <- readFile fn
+                     return $ extractRules file
+
+getRules :: IO [String]
+getRules = do fs <- getFiles "../../../ampersand/src/Database/Design/Ampersand/Input/ADL1/" ".hs"
+              rules <- mapM getFileRules fs
+              return $ concat rules
+
 main :: IO ()
-main = do rules <- readFile "../../../ampersand-abi/Ebnf/ADL.ebnf"
-          let tex = map printEbnfRule (lines rules)
+main = do rules <- getRules
+          let tex = map printEbnfRule rules
+          writeFile "ADL.ebnf" (unlines rules)
           writeFile "GenEbnf.tex" (unlines tex)
           return ()
