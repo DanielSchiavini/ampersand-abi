070fdaca9b019130cd9aa2991a02b09b29721959
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-06-13 10:31:47 +0200

    Haddock generated LaTeX cannot be used inside another document

--- Generated/Haddock/Generate.bat
+++ Generated/Haddock/Generate.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\ampersand\src
 haddock Database\Design\Ampersand\Input\ADL1\Parser --html -o ../../ampersand-abi/Generated/Haddock/html/
-haddock Database\Design\Ampersand\Input\ADL1\Parser --latex -o ../../ampersand-abi/Documentation/Haddock/
+@REM haddock Database\Design\Ampersand\Input\ADL1\Parser --latex -o ../../ampersand-abi/Documentation/Haddock/
 pause
