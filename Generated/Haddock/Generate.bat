0912d0b6e7dccd5f0a7ca8ce1b5b90cbc85efe60
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-06-13 10:28:38 +0200

    Moving all generated artifacts to a separate folder

---
+++ Generated/Haddock/Generate.bat
@@ -0,0 +1,4 @@
+cd ..\..\..\ampersand\src
+haddock Database\Design\Ampersand\Input\ADL1\Parser --html -o ../../ampersand-abi/Generated/Haddock/html/
+haddock Database\Design\Ampersand\Input\ADL1\Parser --latex -o ../../ampersand-abi/Documentation/Haddock/
+pause
